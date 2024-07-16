dc9407e9e22b84e94b0614d9b2ab2d9e72cb3175
yohanfaj <yohan.fajerman@efrei.net>
2024-07-16 11:34:18 +0200

    first version of the dataviz page

--- pages/3 - Data Visualisation.py
+++ pages/3 - Data Visualisation.py
@@ -1,5 +1,7 @@
 # DATAVIZ PAGE
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.set_page_config(
     page_title="Data Visualisation",
@@ -13,8 +15,25 @@ except KeyError: # catching the error when no file has been registered
     # Warning when the user has not uploaded a file yet 
     st.subheader(":warning: **Please upload your dataset first in the landing page!**")
 else: # case when the file has been uploaded
-    st.write(st.session_state)
-    st.write(df.head())
+    st.header("Histograms")
+    select_histo = st.selectbox("**Please select the variable whose histogram you want to display and press the button**",
+                                 df.columns)
+    histo_ok_btn = st.button("Plot histogram")
+
+    if histo_ok_btn:
+        fig = px.histogram(df, x=select_histo)
+        st.plotly_chart(fig)
+
+    st.markdown("---")
+
+    st.header("Boxplots")
+    select_box = st.selectbox("**Please select the variable whose boxplot you want to display and press the button**",
+                                 df.columns)
+    box_ok_btn = st.button("Plot boxplot")
+
+    if box_ok_btn:
+        fig = px.box(df, x=select_box)
+        st.plotly_chart(fig)
 
 
 # Sidebar
