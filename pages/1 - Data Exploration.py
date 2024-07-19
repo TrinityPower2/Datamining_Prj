4674c60338e9787b3bbfa0ce16ce1f9362bcb167
TrinityPower2 <capella.jb2@gmail.com>
2024-07-19 11:25:48 +0200

    Updates: - fixed small issue on page 1 (row & column count not correctly implemented) - added comments on page 1 & 2

--- pages/1 - Data Exploration.py
+++ pages/1 - Data Exploration.py
@@ -1,4 +1,5 @@
 # DATA EXPLORATION PAGE
+import pandas
 import streamlit as st
 
 st.set_page_config(
@@ -13,13 +14,17 @@ except KeyError: # catching the error when no file has been registered
     # Warning when the user has not uploaded a file yet 
     st.subheader(":warning: **Please upload your dataset first in the landing page!**")
 else: # case when the file has been uploaded
-    nb_rows = df.count(0)[0]
-    nb_cols = df.count(1)[0]
+    # count number of rows & columns + add the na containing ones
+    nb_rows = df.count(0).iloc[0] + df.isnull().sum().iloc[0]
+    nb_cols = df.count(1).iloc[0] + df.isnull().sum(axis=1).iloc[0]
+    # number of na values per column
     nb_na_cols = df.isnull().sum()
-    st.write("Number of rows = " + str(nb_rows))
-    st.write("Number of columns = " + str(nb_cols))
+    # display
+    st.write("Number of rows (including na) = " + str(nb_rows))
+    st.write("Number of columns (including na) = " + str(nb_cols))
     st.write("Number of NA per column")
     st.write(nb_na_cols)
+    # describe all columns to obtain basic stats
     st.write("Basic statistics")
     st.write(df.describe(include="all"))
 
--- pages/2 - Pre-processing.py
+++ pages/2 - Pre-processing.py
@@ -6,7 +6,9 @@ from sklearn.impute import KNNImputer
 from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler
 from scipy.stats import zscore
 
+
 def delete_na(df_to_clean: pandas.DataFrame, method):
+    # perform the deletion according to sent method
     if method == "Rows":
         df_to_clean.dropna(axis=0, inplace=True, how='any')
     elif method == "Columns":
@@ -14,13 +16,16 @@ def delete_na(df_to_clean: pandas.DataFrame, method):
     elif method == "Both":
         df_to_clean.dropna(axis=0, inplace=True)
         df_to_clean.dropna(axis=1, inplace=True)
+
+    # replace the df by the cleaned version in session state storage
     st.session_state["file"] = df_to_clean
+    # button management
     set_stage(1)
     st.rerun()
 
 
 def replace_na(df_to_clean: pandas.DataFrame, method):
-    # handling numeric columns
+    # handling numeric columns according to sent method
     if method == "Mean":
         df_to_clean.fillna(df_to_clean.mean(numeric_only=True, skipna=True), inplace=True)
     elif method == "Median":
@@ -31,13 +36,15 @@ def replace_na(df_to_clean: pandas.DataFrame, method):
     # handling the remaining non-numeric columns by filling in with the mode
     df_to_clean.fillna(df_to_clean.mode(), inplace=True)
 
+    # replace df by cleaned version in session state storage
     st.session_state["file"] = df_to_clean
+    # button management
     set_stage(1)
     st.rerun()
 
 
 def replace_na_algo(df_to_clean: pandas.DataFrame, method):
-
+    # replace na according to sent method
     if method == "KNN":
         knn_imputer = KNNImputer(n_neighbors=1)
         for i in df_to_clean:
@@ -46,40 +53,49 @@ def replace_na_algo(df_to_clean: pandas.DataFrame, method):
     elif method == "Simple imputation":
         df_to_clean.interpolate(inplace=True)
 
+    # replace df by cleaned version in session state storage
     st.session_state["file"] = df_to_clean
+    # button management
     set_stage(1)
     st.rerun()
 
 
 def min_max_norm(df_to_norm):
+    # min max normalize the data
     normalizer = MinMaxScaler(copy=False)
     for i in df_to_norm:
         if df[i].dtype != object:
             df[i] = normalizer.fit_transform(pandas.DataFrame(df[i]))
 
+    # replace df by cleaned version in session state storage
     st.session_state["file"] = df_to_norm
     st.rerun()
 
 
 def z_score_norm(df_to_norm):
+    # z_score normalize the data
     for i in df_to_norm:
         if df[i].dtype != object:
             df[i] = zscore(df[i])
 
+    # replace df by cleaned version in session state storage
     st.session_state["file"] = df_to_norm
     st.rerun()
 
 
 def max_abs_norm(df_to_norm):
+    # max_abs normalize the data
     normalizer = MaxAbsScaler(copy=False)
     for i in df_to_norm:
         if df[i].dtype != object:
             df[i] = normalizer.fit_transform(pandas.DataFrame(df[i]))
 
+    # replace df by cleaned version in session state storage
     st.session_state["file"] = df_to_norm
     st.rerun()
 
 
+# multiple levels of buttons management
 def set_stage(i):
     st.session_state["stage"] = i
 
@@ -96,19 +112,26 @@ except KeyError: # catching the error when no file has been registered
     # Warning when the user has not uploaded a file yet 
     st.subheader(":warning: **Please upload your dataset first in the landing page!**")
 else:  # case when the file has been uploaded
+
+    # display currently stored data
     st.subheader("Current state of df")
     st.write(df)
+
+    # if not, create & initialize the stage variable (used for button management) in sessions state storage
     if 'stage' not in st.session_state:
         set_stage(1)
 
+    # data cleaning choice & inputs
     st.header("NA values cleaning")
 
+    # data cleaning choice
     na_cleaning_list = ["Delete rows and/or columns with missing values",
                         "Replace missing values with mean/median/mode",
                         "Use a sophisticated imputation algorithm"]
     select_output = st.selectbox("**Please select your NA values cleaning method and press OK**",
                                  na_cleaning_list)
     if st.button("OK") or st.session_state["stage"] == 2:
+        # delete na inputs
         if select_output == "Delete rows and/or columns with missing values":
             st.subheader(select_output)
 
@@ -116,9 +139,11 @@ else:  # case when the file has been uploaded
             select_output = st.selectbox("**Please select where to delete the NA values and press Start**",
                                          possibility_list)
 
+            # cleaning start
             if st.button("Start", key="St1", on_click=set_stage(2)):
                 delete_na(df, select_output)
 
+        # replace na inputs
         elif select_output == "Replace missing values with mean/median/mode":
             st.subheader(select_output)
 
@@ -126,9 +151,12 @@ else:  # case when the file has been uploaded
             select_output = st.selectbox("**Please select the method to use (mode will always be used for non-numerical"
                                          " columns) and press start**",
                                          possibility_list)
+
+            # cleaning start
             if st.button("Start", key="St2", on_click=set_stage(2)):
                 replace_na(df, select_output)
 
+        # replace algo na inputs
         elif select_output == "Use a sophisticated imputation algorithm":
             st.subheader(select_output)
 
@@ -136,11 +164,14 @@ else:  # case when the file has been uploaded
             select_output = st.selectbox("**Please select the method to use and press start (note that only NaN from "
                                          "numeric columns will be treated)**",
                                          possibility_list)
+
+            # cleaning start
             if st.button("Start", key="St3", on_click=set_stage(2)):
                 replace_na_algo(df, select_output)
         else:
             st.write("Unknown method")
 
+    # normalization method choice
     st.header("Data normalization")
 
     normalization_list = ["Min-max normalization",
@@ -148,6 +179,8 @@ else:  # case when the file has been uploaded
                           "MaxAbs normalization"]
     select_output = st.selectbox("**Please select your normalization method and press OK**",
                                  normalization_list)
+
+    # normalization start
     if st.button("OK", key="OK2"):
         if select_output == "Min-max normalization":
             min_max_norm(df)
